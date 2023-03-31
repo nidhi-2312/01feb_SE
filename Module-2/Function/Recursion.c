e7bc8f365cebcb11d07ff2f259abba7063d4e3bb
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-03-31 11:38:51 +0530

    add

---
+++ Module-2/Function/Recursion.c
@@ -0,0 +1,23 @@
+// WAP To find factorial  using recursive function
+#include<stdio.h>
+// Recursive function
+long double fecto(long double no)
+{
+    if (no < 1)
+    {
+        return 1;
+    }
+    // fecto() method call it self
+    return  no * fecto(no-1);
+    /*
+        for ex.
+    */
+}
+int main()
+{
+    long double n;
+    printf("Enter the value of no:");
+    scanf("%Lf", &n);
+    printf("%Lf",fecto(n));
+    return 0;
+}
