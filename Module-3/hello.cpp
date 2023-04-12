390664ba2492efcb733ae6e5f695f4fffe95787d
tops-nidhi <109589571+tops-nidhi@users.noreply.github.com>
2023-04-12 11:10:45 +0530

    add

--- Module-3/hello.cpp
+++ Module-3/hello.cpp
@@ -1,10 +1,15 @@
+// WAP to get the value from user
 #include<iostream>
+// Namespace is a library file
 using namespace std;
 int main()
 {
     int no;
+    // use to pass the message
     cout<<"\nEnter the value of no:";
+    // Use to store the value which was given by user
     cin>>no;
+    // 'endl' use to end the line
     cout<<endl<<"Value of no"<<no <<"is:";
     return 0;
 }
